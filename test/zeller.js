ad2b218e8ea053c84bec35e192e1ad177f4ca59c
Joshua Philips <jmichaelphilips@gmail.com>
2016-08-30 12:02:40 -0500

    added some logic to the zeller module

--- test/zeller.js
+++ test/zeller.js
@@ -5,13 +5,13 @@ const { assert: {isFunction} } = require('chai');
 const { zellerCon } = require('../lib/zeller');
 
 const d = new Date();
+
 let thisDate = { month: d.getMonth(),
                  day: d.getDate(), 
                  year: d.getFullYear(),
                  dayOfWeek: d.getDay()
                 
 };
-
 
 describe('zellerCon', () => {
     it('should be a function', () => {
