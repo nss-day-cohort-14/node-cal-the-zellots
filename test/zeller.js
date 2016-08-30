3b25a7474278f76f8bfe58e9fdd41a274b88f5e0
Joshua Philips <jmichaelphilips@gmail.com>
2016-08-30 15:47:00 -0500

    Made some tests pass.  Failed some other tests.

--- test/zeller.js
+++ test/zeller.js
@@ -1,22 +1,46 @@
 'use strict'
 
-const { assert: {isFunction} } = require('chai');
+const { assert: {isFunction, oneOf, strictEqual} } = require('chai');
 
-const { zellerCon } = require('../lib/zeller');
+const { zellerCon, modMonth, modYear } = require('../lib/zeller');
 
-const d = new Date();
-
-let thisDate = { month: d.getMonth(),
-                 day: d.getDate(), 
-                 year: d.getFullYear(),
-                 dayOfWeek: d.getDay()
-                
-};
-
-describe('zellerCon', () => {
+describe('modMonth', () => {
     it('should be a function', () => {
-        console.log(thisDate)
-        isFunction(zellerCon);
+        isFunction(modMonth)
+    });
+    it('1 should return 13', () => {
+        strictEqual(modMonth(1), 13)
+    });
+    it('2 should return 14', () => {
+        strictEqual(modMonth(2), 14);
+    })
+});
+
+describe('modYear', () => {
+    it('should be a function', () => {
+        isFunction(modYear);
+    });
+    it('(2011, 1) should return 2010', () => {
+        strictEqual(modYear(2011, 1), 2010 )
+    });
+     it('(2018, 2) should return 2017', () => {
+        strictEqual(modYear(2018, 2), 2017 )
+    });
+    it('(2019, 3) should return 2019', () => {
+        strictEqual(modYear(2019, 3), 2019)
     })
 })
 
+
+describe('zellerCon', () => {
+    it('should be a function', () => {
+        isFunction(zellerCon);
+    });
+    it('should return a number between 1 & 7', () => {
+        const expected = [1,2,3,4,5,6,7]        
+        oneOf(zellerCon(2016, 1, 1), expected)
+    });
+    it('(2016, 1, 1) should return 5', () => {
+        strictEqual(zellerCon(2016, 1, 1), 5)
+    })
+});
